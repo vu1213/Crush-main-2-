877a68426e0ce4333b494226e70dc61b2db7c7f6
vu1213 <85048996+vu1213@users.noreply.github.com>
2022-11-14 23:45:46 +0700

    upload source code

---
+++ Crush-main/config.js
@@ -0,0 +1,14 @@
+const CONFIG = {
+    titleWeb: "HAPPY BIRTHDAY",
+    introTitle: 'HÊ LÔ BÉ THÌU HỜM',
+    introDesc: `Hông bít nói gì nên xem tiếp đi ha`,
+    btnIntro: '^^HeHe^^',
+    title: 'HAPPY BIRTHDAY',
+    desc: 'Yêu thương nhiều lắm ',
+    btnYes: 'ò yêu nhiều lắm',
+    btnNo: 'khôm yêu',
+    question: 'Happy birthday thiên thần nhỏ Thìu Hờm nhó ,chỉ muốn nói chúc cậu luôn xinh đẹp,vui vẻ ,ngày càng thành công hơn trong cuộc sống. Chúc sang tủi mới kiếm được nhiều tiền ,được nhiều người quý mến, trưởng thành hơn bớt trẻ trâu đi, nói thì nối vậy chứ tui yêu sự trẻ trâu của bạn :>> ',
+    btnReply: 'NEXT',
+    reply: 'NEXT',
+    mess: 'Iu thương vô bờ bến <3',
+}
